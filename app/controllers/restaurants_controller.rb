33a0aafa41736859c8b7f1e0bf27051c3b8fe04b
jiajie li <jackylee1989@hotmail.com>
2018-07-24 08:40:31 +0800

    rails new

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,11 @@
+class RestaurantsController < ApplicationController
+
+  RESTAURANTS = {
+    1 => { name: "Dishoom", address: "Shoreditch, London", category: "indian" },
+    2 => { name: "Sushi Samba", address: "City, London", category: "japanese" }
+  }
+
+  def index
+    @restaurants = Restaurant.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'about', to: 'pages#about', as: :about
+  get 'contact', to: 'pages#contact', as: :contact
+  get 'restaurants', to: 'restaurants#index'
+
+  root to: 'pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
